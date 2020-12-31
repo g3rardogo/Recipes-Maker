00c27743a39e8adbb5202358c925d2b0977e9c93
Gerardo Ramirez <gerardogo145@gmail.com>
2020-12-31 12:43:43 +0100

    Creando funcion que solicita al usuario la cantidad del ingrediente

--- src/main/kotlin/model/Aceite.kt
+++ src/main/kotlin/model/Aceite.kt
@@ -1,13 +1,12 @@
 package model
 
 class Aceite(): Product() {
-    val menu = listOf<String>("1. Aceite de girasol", "2. Aceite de oliva", "3. Aceite de coco", "4. Aceite de palma")
+    val menu = listOf<String>("Aceite de girasol", "Aceite de oliva", "Aceite de coco", "Aceite de palma")
     //Cambiar a int
     override fun create(response: String) {
         ingredientes.add(menu[response.toInt() - 1])
         //println(menu[response.toInt() - 1]
-        println(ingredientes)
-        menu()
+        cantidad(menu[response.toInt() - 1])
     }
 
     fun menu(){
@@ -15,9 +14,17 @@ class Aceite(): Product() {
             println(ingredient)
         }
         val response: String? = readLine()
-        println(response)
         if (response != null) {
             create(response)
         }
+    }
+
+    fun cantidad(ingrediente: String){
+        println("Ingrese la cantidad de $ingrediente")
+        val response: String? = readLine()
+        ingredientes.add(response.toString())
+        println(ingredientes)
+        println("Ingrediente agregado exitosamente")
+        menu()
     }
 }
